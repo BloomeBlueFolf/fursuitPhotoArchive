80d6879d8497a2a62b6e14f51366296c5b8d7347
fege <f.ege@web.de>
2022-10-20 21:02:11 +0200

    login security from tutorial #1

--- src/main/java/com/archive/fursuit/services/interfaces/UserServiceInterface.java
+++ src/main/java/com/archive/fursuit/services/interfaces/UserServiceInterface.java
@@ -2,8 +2,9 @@ package com.archive.fursuit.services.interfaces;
 
 import com.archive.fursuit.User;
 import com.archive.fursuit.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserServiceInterface {
+public interface UserServiceInterface extends UserDetailsService {
 
     User saveUser(UserRegistrationDto userRegistrationDto);
 }
